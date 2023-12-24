6f7c2475961f00ee2dace104bd74987228891bc3
merabza <merabza@gmail.com>
2023-12-24 20:00:34 +0400

    202312242000

--- LibFileParameters/Models/FileStorageData.cs
+++ LibFileParameters/Models/FileStorageData.cs
@@ -41,27 +41,27 @@ public sealed class FileStorageData : ItemData
         return uri.Scheme.ToLower() == "ftp";
     }
 
-    public static bool IsSameToLocal(FileStorageData forFileStorage, string localPath,
-        IMessagesDataManager? messagesDataManager, string? userName)
+    public static bool IsSameToLocal(FileStorageData forFileStorage, string localPath)
+        //,IMessagesDataManager? messagesDataManager, string? userName)
     {
         var fileStoragePath = forFileStorage.FileStoragePath;
         //ან თუ წყაროს ფაილსაცავი ლოკალურია და მისი ფოლდერი ემთხვევა პარამეტრების ლოკალურ ფოლდერს.
         //   მაშინ მოქაჩვა საჭირო აღარ არის
         if (fileStoragePath is null || string.IsNullOrWhiteSpace(localPath))
         {
-            messagesDataManager?.SendMessage(userName,
-                "IsSameToLocal Returns false because forFileStorage is null or localPath empty ",
-                CancellationToken.None).Wait();
-            Console.WriteLine("IsSameToLocal Returns false because forFileStorage is null or localPath empty ");
+            //messagesDataManager?.SendMessage(userName,
+            //    "IsSameToLocal Returns false because forFileStorage is null or localPath empty ",
+            //    CancellationToken.None).Wait();
+            //Console.WriteLine("IsSameToLocal Returns false because forFileStorage is null or localPath empty ");
             return false;
         }
 
         if (FileStat.IsFileSchema(fileStoragePath))
             return FileStat.NormalizePath(localPath) == FileStat.NormalizePath(fileStoragePath);
 
-        messagesDataManager?.SendMessage(userName,
-            $"IsSameToLocal Returns true because {fileStoragePath} is not file schema", CancellationToken.None).Wait();
-        Console.WriteLine($"IsSameToLocal Returns true because {fileStoragePath} is not file schema", fileStoragePath);
+        //messagesDataManager?.SendMessage(userName,
+        //    $"IsSameToLocal Returns true because {fileStoragePath} is not file schema", CancellationToken.None).Wait();
+        //Console.WriteLine($"IsSameToLocal Returns true because {fileStoragePath} is not file schema", fileStoragePath);
         return false;
     }
 }
