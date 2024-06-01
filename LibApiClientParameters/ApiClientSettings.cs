99a0774680c9fe92ea54533fb0b8c76bb191827b
merabza <merabza@gmail.com>
2024-06-01 17:04:43 +0400

    202406011704

--- LibApiClientParameters/ApiClientSettings.cs
+++ LibApiClientParameters/ApiClientSettings.cs
@@ -5,6 +5,7 @@ namespace LibApiClientParameters;
 public sealed class ApiClientSettings : ItemData
 {
     public string? Server { get; set; }
+
     public string? ApiKey { get; set; }
     //public bool WithMessaging { get; set; }
 }
