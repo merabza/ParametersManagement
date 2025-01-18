ede576423a7b866e97e08e1012905211ab5d81dd
merabza <merabza@gmail.com>
2025-01-18 20:22:44 +0400

    202501182022

---
+++ LibDatabaseParameters/DatabasesParameters.cs
@@ -0,0 +1,37 @@
+﻿using LibParameters;
+
+namespace LibDatabaseParameters;
+
+public sealed class DatabasesParameters : ParametersWithStatus
+{
+    //public EDataProvider DataProvider { get; set; }
+
+    //შემდეგი 2 პარამეტრიდან გამოიყენება ერთერთი
+    //ორივეს ერთდროულად შევსება არ შეიძლება.
+    //ორივეს ცარელა დატოვება არ შეიძლება
+    //მონაცემთა ბაზასთან კავშირის სახელი
+    public string? DbConnectionName { get; set; }
+
+    ////მონაცემთა ბაზასთან დამაკავშირებელი ვებაგენტის სახელი
+    //public string? DbWebAgentName { get; set; }
+
+    ////ვებაგენტის მხარეს მონაცემთა ბაზასთან დამაკავშირებელი სახელი
+    //public string? RemoteDbConnectionName { get; set; }
+
+    public string? DbServerFoldersSetName { get; set; }
+
+    public string? DatabaseName { get; set; }
+
+    ////ეს პარამეტრები საჭიროა, როცა ბაზა არის ფაილის ტიპის. მაგალითად, SQLite ან აქსესის ბაზებისთვის
+    //public string? DatabaseFilePath { get; set; }
+    //public string? DatabasePassword { get; set; }
+
+    //ჭკვიანი სქემის სახელი. გამოიყენება ძველი დასატოვებელი და წასაშლელი ფაილების განსასაზღვრად. (ბაზის სერვერის მხარეს)
+    //ბაზის სერვერის მხარეს არსებული ჭკვიანი სქემის სახელებიუს მოტვირთვა შესაძლებელი უნდა იყოს რედაქტირების დროს
+    public string? SmartSchemaName { get; set; }
+
+    //სერვერის მხარეს ფაილსაცავის ნიკი
+    //ბაზის ბექაპების გაცვლისათვის გამოსადეგი ფაილსაცავების ნიკების მოტვირთვა შესაძლებელი უნდა იყოს რედაქტირების დროს
+    public string? FileStorageName { get; set; }
+    public int CommandTimeOut { get; set; }
+}
