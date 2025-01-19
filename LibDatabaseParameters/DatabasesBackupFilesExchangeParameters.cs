4f24b34cae11f45031db933eebdf0dcac084b052
merabza <merabza@gmail.com>
2025-01-19 12:54:50 +0400

    202501191254

---
+++ LibDatabaseParameters/DatabasesBackupFilesExchangeParameters.cs
@@ -0,0 +1,22 @@
+﻿using LibParameters;
+
+namespace LibDatabaseParameters;
+
+public sealed class DatabasesBackupFilesExchangeParameters : ParametersWithStatus
+{
+    //ჩამოტვირთვისა და ატვირთვის დროებითი გაფართოებები
+    public string? DownloadTempExtension { get; set; }
+    public string? UploadTempExtension { get; set; }
+
+    //გაცვლის ფაილსაცავის სახელი
+    public string? ExchangeFileStorageName { get; set; }
+
+    //ჭკვიანი სქემის სახელი. გამოიყენება ძველი დასატოვებელი და წასაშლელი ფაილების განსასაზღვრად. (ეს რეზერვაციის ფაილსაცავის მხარეს)
+    public string? ExchangeSmartSchemaName { get; set; }
+
+    //ლოკალური ფოლდერი
+    public string? LocalPath { get; set; }
+
+    //ჭკვიანი სქემის სახელი. გამოიყენება ძველი დასატოვებელი და წასაშლელი ფაილების განსასაზღვრად. (ეს ლოკალური ფოლდერის მხარეს)
+    public string? LocalSmartSchemaName { get; set; }
+}
