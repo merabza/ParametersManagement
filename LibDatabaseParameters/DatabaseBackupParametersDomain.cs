b56db9776d4ce1064609b6da6636fe823462064d
merabza <merabza@gmail.com>
2025-03-20 23:54:36 +0400

    202503202353

--- LibDatabaseParameters/DatabaseBackupParametersDomain.cs
+++ LibDatabaseParameters/DatabaseBackupParametersDomain.cs
@@ -1,4 +1,5 @@
-﻿using DbTools;
+﻿using System;
+using DbTools;
 using LibParameters;
 using SystemToolsShared;
 
@@ -34,23 +35,24 @@ public sealed class DatabaseBackupParametersDomain : IParameters
         return true;
     }
 
-    //public static DatabaseBackupParametersDomain Create(DatabaseBackupParametersModel? dbBackupParameters)
-    //{
-    //    var computerName = Environment.MachineName;
+    //Create გამოიყენება ApAgent-ში
+    public static DatabaseBackupParametersDomain Create(DatabaseParameters? dbBackupParameters)
+    {
+        var computerName = Environment.MachineName;
 
-    //    return new DatabaseBackupParametersDomain(
-    //        GetValueOrDefault(dbBackupParameters?.BackupNamePrefix, $"{computerName}_"),
-    //        GetValueOrDefault(dbBackupParameters?.DateMask, "yyyyMMddHHmmss"),
-    //        GetValueOrDefault(dbBackupParameters?.BackupFileExtension, ".bak"),
-    //        GetValueOrDefault(dbBackupParameters?.BackupNameMiddlePart, "_FullDb_"),
-    //        dbBackupParameters?.Compress ?? true, dbBackupParameters?.Verify ?? true,
-    //        dbBackupParameters?.BackupType ?? EBackupType.Full);
-    //}
+        return new DatabaseBackupParametersDomain(
+            GetValueOrDefault(dbBackupParameters?.BackupNamePrefix, $"{computerName}_"),
+            GetValueOrDefault(dbBackupParameters?.DateMask, "yyyyMMddHHmmss"),
+            GetValueOrDefault(dbBackupParameters?.BackupFileExtension, ".bak"),
+            GetValueOrDefault(dbBackupParameters?.BackupNameMiddlePart, "_FullDb_"),
+            dbBackupParameters?.Compress ?? true, dbBackupParameters?.Verify ?? true,
+            dbBackupParameters?.BackupType ?? EBackupType.Full);
+    }
 
-    //private static string GetValueOrDefault(string? val, string def)
-    //{
-    //    return string.IsNullOrWhiteSpace(val) ? def : val;
-    //}
+    private static string GetValueOrDefault(string? val, string def)
+    {
+        return string.IsNullOrWhiteSpace(val) ? def : val;
+    }
 
     public string GetPrefix(string databaseName)
     {
