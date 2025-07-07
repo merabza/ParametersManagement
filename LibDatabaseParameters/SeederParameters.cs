af5d60bfcf62fb4a1fc4c0f66a7ddfe398b512b0
merabza <merabza@gmail.com>
2025-07-08 00:44:32 +0400

    202507080044

--- LibDatabaseParameters/SeederParameters.cs
+++ LibDatabaseParameters/SeederParameters.cs
@@ -7,7 +7,9 @@ public sealed class SeederParameters : IParameters
     public string? JsonFolderName { get; set; }
     public string? SecretDataFolder { get; set; }
     public string? LogFolder { get; set; }
-    public EDatabaseProvider DataProvider { get; set; }
+    public string? ExcludesRulesParametersFilePath { get; set; }
+
+    //public EDatabaseProvider DataProvider { get; set; }
     public string? ConnectionStringSeed { get; set; }
 
     public bool CheckBeforeSave()
