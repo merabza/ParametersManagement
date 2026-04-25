b404489178466fb96860e6d562f0d91b67b340f4
merabza <merabza@gmail.com>
2026-04-25 21:01:53 +0400

    202604252059

---
+++ ParametersManagement.LibParameters/DependencyInjection/ParametersManagerServicesExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ParametersManagement.LibParameters.DependencyInjection;
+
+public static class ParametersManagerServicesExtensions
+{
+    // ReSharper disable once UnusedMethodReturnValue.Global
+    public static IServiceCollection AddMainParametersManager(this IServiceCollection services,
+        Action<MainParametersManagerOptions> setupAction)
+    {
+        services.AddSingleton<IParametersManager, ParametersManager>();
+        services.Configure(setupAction);
+        return services;
+    }
+}
