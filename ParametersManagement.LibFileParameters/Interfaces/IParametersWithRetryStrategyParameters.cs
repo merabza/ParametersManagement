01fdf31d428fb0dca199e7d7dcdbaa5fb80d0038
merabza <merabza@gmail.com>
2026-05-08 09:55:20 +0400

    202605080954

---
+++ ParametersManagement.LibFileParameters/Interfaces/IParametersWithRetryStrategyParameters.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using ParametersManagement.LibFileParameters.Models;
+using ParametersManagement.LibParameters;
+
+namespace ParametersManagement.LibFileParameters.Interfaces;
+
+public interface IParametersWithRetryStrategyParameters : IParameters
+{
+    Dictionary<string, RetryStrategyParameters> RetryStrategyParameters { get; }
+}
---
+++ ParametersManagement.LibFileParameters/Models/RetryStrategyParameters.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using Polly;
+using SystemTools.SystemToolsShared;
+
+namespace ParametersManagement.LibFileParameters.Models;
+
+public class RetryStrategyParameters : ItemData
+{
+    [Range(1, int.MaxValue)] public int MaxRetryAttempts { get; set; }
+
+    public DelayBackoffType BackoffType { get; set; }
+
+    public bool UseJitter { get; set; }
+
+    [Range(typeof(TimeSpan), "00:00:00", "1.00:00:00")]
+    public TimeSpan Delay { get; set; }
+
+    [Range(typeof(TimeSpan), "00:00:00", "1.00:00:00")]
+    public TimeSpan? MaxDelay { get; set; }
+
+    public override string GetItemKey()
+    {
+        return $"{MaxRetryAttempts} {BackoffType} {UseJitter} {Delay} {MaxDelay}";
+    }
+}
